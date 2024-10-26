539a26d0411407b6e8cb5b5cc0c554b61f466c0d
Lucas Paiva <lcspaiva87@gmail.com>
2024-10-26 19:48:54 -0300

    Refactor input fields to use custom TextArea component

--- src/app/home/_components/StudentRegistration.tsx
+++ src/app/home/_components/StudentRegistration.tsx
@@ -3,6 +3,7 @@ import { Upload, UserPlus, FileSpreadsheet, Check, X } from 'lucide-react';
 import { classData } from '@/app/mock/data';
 import { Input } from '@/components/Inputs';
 import { Select } from '@/components/Select';
+import { Textarea } from '@/components/TextArea';
 
 
 interface StudentForm {
@@ -191,12 +192,11 @@ const StudentRegistration: React.FC = () => {
                   <label className="block text-sm font-medium text-gray-700 mb-1">
                     Allergies
                   </label>
-                  <textarea
+                  <Textarea
                     name="allergies"
                     value={form.allergies}
                     onChange={handleInputChange}
                     rows={2}
-                    className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-indigo-500 focus:border-indigo-500"
                     placeholder="List any allergies or write 'None' if not applicable"
                   />
                 </div>
